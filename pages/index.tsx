610fb8e230a6ee500dbb6bd335ea20c3c68bb97e
Hannan Rozal <75463261+hannanrozal@users.noreply.github.com>
2023-05-07 09:49:28 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Harry Potter. How can I help you?`
+        content: `Hi there! I'm Ash Ketchum. How can I help you?`
       }
     ]);
   };
@@ -93,7 +93,7 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Harry Potter. How can I help you?`
+        content: `Hi there! I'm Ash Ketchum. How can I help you?`
       }
     ]);
   }, []);
